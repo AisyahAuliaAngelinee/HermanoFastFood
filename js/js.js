de76eec9d24da1b7a51729ad07ae1dda4c0688d6
Aisyah Aulia Angelinee <127939867+AisyahAuliaAngelinee@users.noreply.github.com>
2023-09-16 11:57:03 +0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
-let navbar = document.getElementsByTagName('nav')[0]
+// let navbar = document.getElementsByTagName('nav')[0]
 
 // console.log(document.getElementsByClassName('icons'));
 // console.log(document.getElementById('menu'));
 // document.querySelector('#menu-btn').addEventListener('click', function ()  {
 //     console.log('test');
 //     navbar.classList.toggle('active');
-// }) 
+// })
 
 // = () => {
 //     navbar.classList.toggle('active');
@@ -13,11 +13,17 @@ let navbar = document.getElementsByTagName('nav')[0]
 
 // console.log(header)
 // console.log(flex)
-console.log(navbar)
-document.querySelector('#menu').onclick = () => {
-    navbar.classList.toggle('active');
-}
+// console.log(navbar)
+// document.querySelector('#menu').onclick = () => {
+//     navbar.classList.toggle('active');
+// }
 
 // document.querySelector('#menu').onclick = () => {
 //     document.getElementById('menu').classList.toggle('active');
 // }
+
+let navbar = document.querySelector(".header .flex .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
